384fcb2f61212be006e014dce598cdc392c7fc25
Francis Ong <francis.ong25@gmail.com>
2022-03-09 22:24:27 +0800

    relocate getPrice, and getIcon and pass to components

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-   <header class="top-bar spread">
+  <header class="top-bar spread">
     <nav class="top-bar-nav">
       <router-link to="/" class="top-bar-link">
         <i class="icofont-spoon-and-fork"></i>
@@ -12,10 +12,83 @@
         <span>Past Orders</span>
       </router-link>
     </nav>
-    <!-- <a href="#" class="top-bar-cart-link">
+    <a @click="toggleSideBar" class="top-bar-cart-link">
       <i class="icofont-cart-alt icofont-1x"></i>
-      <span>Cart (0)</span>
-    </a> -->
+      <span>Cart ({{ totalQuantity }})</span>
+    </a>
   </header>
-  <router-view/>
+
+  <router-view
+    :inventory="food"
+    :addToCart="addToCart"
+    :orders="orders"
+    :getPrice="getPrice"
+    :getIcon="getIcon"
+  />
+
+  <SideBar
+    v-if="showSideBar"
+    :toggle="toggleSideBar"
+    :cart="cart"
+    :remove="removeItem"
+    :getPrice="getPrice"
+    :getIcon="getIcon"
+    :checkOut="checkOut"
+  />
+
 </template>
+
+<script>
+import food from './food.json'
+import SideBar from '@/components/SideBar.vue'
+
+export default {
+  components: {
+    SideBar
+  },
+  data () {
+    return {
+      showSideBar: false,
+      cart: {},
+      orders: {},
+      food: food
+    }
+  },
+  computed: {
+    totalQuantity () {
+      return Object.values(this.cart).reduce((total, value) => {
+        return total + value
+      }, 0)
+    }
+  },
+  methods: {
+    toggleSideBar () {
+      this.showSideBar = !this.showSideBar
+    },
+    addToCart (name, index) {
+      if (!this.cart[name]) this.cart[name] = 0
+      this.cart[name] += this.food[index].quantity
+      this.food[index].quantity = 0 // reset
+    },
+    removeItem (name) {
+      delete this.cart[name]
+    },
+    getPrice (name) {
+      const product = this.food.find((p) => {
+        return p.name === name
+      })
+      return product.price.USD
+    },
+    getIcon (name) {
+      const product = this.food.find((p) => {
+        return p.name === name
+      })
+      return product.icon
+    },
+    checkOut () {
+      this.orders = { ...this.orders, ...this.cart }
+      this.cart = {}
+    }
+  }
+}
+</script>
